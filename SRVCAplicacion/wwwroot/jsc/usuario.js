8f6f5c2558a4063ceb63de8aa635b5ec8adf6004
AxelDB96 <baldovinoaxel757@gmail.com>
2024-11-05 22:28:20 -0300

    Agregue funciones a usuario.js 05/11

--- SRVCAplicacion/wwwroot/jsc/usuario.js
+++ SRVCAplicacion/wwwroot/jsc/usuario.js
@@ -47,3 +47,59 @@ async function mostrarUsuarios() {
         console.error('Error al mostrar usuarios:', error);
     }
 }
+
+//Funcion para cargar los datos del usuario elegido(admin)
+
+async function cargarDatosUsuario(id_usuario) {
+    try {
+        const response = await fetch('');
+        if (!response.ok) throw new Error('Error al intentar cargar los datos del usuario');
+
+        const usuario = await response.json();
+
+        document.getElementById('id_usuario').value = usuario.id_usuario;
+        document.getElementById('usuario').value = usuario.usuario;
+        document.getElementById('contraseña').value = usuario.contraseña;
+        document.getElementById('email').value = usuario.email;
+        document.getElementById('telefono').value = usuario.telefono;
+        document.getElementById('dni').value = usuario.dni;
+        document.getElementById('estado').value = usuario.estado;
+        document.getElementById('id_punto_control').value = usuario.id_punto_control;
+    } catch (error) {
+        console.error('Error al intentar cargar los datos del usuario', error);
+    }
+}
+
+//Funcion para guardar los cambios del usuario elegido(admin)
+
+async function guardarCambiosUsuario() {
+    const id_usuario = document.getElementById('id_usuario').value;
+    const usuarioEditado = {
+        Nombre: document.getElementById('usuario').value,
+        Contraseña: document.getElementById('contraseña').value;
+        EMAIL: document.getElementById('email').value;
+        Telefono: document.getElementById('telefono').value;
+        DNI: document.getElementById('dni').value;
+        Estado: document.getElementById('estado').value;
+        ID punto de Control: document.getElementById('id_punto_control').value
+    };
+    try {
+        const response = await fetch('api', {
+            method: 'PUT',
+            headers: {
+                'Content.Type': 'application/json'
+            },
+            body: JSON.stringify(usuarioEditado)
+        });
+
+        if (response.ok) {
+            alert('Usuario actualizado exitosamente!');
+
+        } else {
+            alert('Error al intentar actualizar el usuario');
+        }
+
+    } catch (error) {
+        console.error('Error al guardar cambios del usuario:', error);
+    }
+}
